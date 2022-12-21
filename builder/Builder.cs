d0fafb40ebdd8768b1081d5a38b4fae41bfc8032
fdsc <fdsc@yandex.ru>
2022-12-21 22:50:32 +0300

    --

--- builder/Builder.cs
+++ builder/Builder.cs
@@ -4,29 +4,56 @@ namespace builder;
 
 partial class Program
 {
-    public static ErrorCodes MainBuild()
+    public static (ErrorCodes resultCode, bool WillTests) MainBuild()
     {
         if (!checkVersionOfBuildProgramm())
         {
-            return ExecuteFullBuild();
+            return (ExecuteFullBuild(), false);
         }
 
-        return ErrorCodes.Success;
+        return (ErrorCodes.Success, true);
     }
 
+    /// <summary>Проверяем, актуальна ли на нынешний момент программа билда</summary>
+    /// <returns>true - если билдер актуален; иначе false</returns>
     public static bool checkVersionOfBuildProgramm()
     {
-        return false;
+        var pathToFile = typeof(Program).Assembly.Location;
+        // Console.WriteLine(pathToFile);
+
+        var di    = new DirectoryInfo(Directory.GetCurrentDirectory());
+        var fi    = new FileInfo(pathToFile);
+        var last  = fi.LastWriteTimeUtc;
+        var files = di.GetFiles("*.cs", SearchOption.AllDirectories);
+
+        foreach (var file in files)
+        {
+            if (file.LastWriteTimeUtc >= last)
+            {
+                // Console.WriteLine($"Update file found: {file.FullName}");
+                updated_file_found_event?.Invoke(file);
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    public delegate void Builder_Lock_Event();
-    public event Builder_Lock_Event builder_lock_event = null;
+    public delegate void  Builder_Lock_Event      (FileInfo builder_lock_file);
+    public delegate void  Updated_File_Found_Event(FileInfo updatedFile);
+
+    public static   event Builder_Lock_Event?       builder_lock_event;
+    public static   event Updated_File_Found_Event? updated_file_found_event;
 
     public static ErrorCodes ExecuteFullBuild()
     {
-        if (File.Exists("builder.lock"))
+        var fi = new FileInfo("builder.lock");
+        if (fi.Exists)
         {
-            
+            var b = builder_lock_event;
+            if (b != null)
+                b(fi);
+
             return ErrorCodes.Builder_Lock;
         }
 
--- builder/Program.cs
+++ builder/Program.cs
@@ -6,28 +6,42 @@ partial class Program
 {
     static int Main(string[] args)
     {
-        Console.WriteLine($"Builder started at {getTimeString(DateTime.Now)}");
+        using (var opt = new NotImportantConsoleOptions())
+        {
+            Console.Write($"Builder started at {getTimeString(DateTime.Now)}");
+        }
+
+        // ---------------- Устанавливаем обработчики ошибок ----------------
+
+        SetErrorHandlers();
 
         // ---------------- Компиляция ----------------
 
         var ec = MainBuild();
-        if (ec != ErrorCodes.Success)
+        if (ec.resultCode != ErrorCodes.Success)
         {
-            Console.WriteLine($"{getTimeString(DateTime.Now)}. Error during build");
-            return (int) ec;
+            Console.Error.WriteLine($"{getTimeString(DateTime.Now)}. Error during build");
+            return (int) ec.resultCode;
         }
+
+        if (!ec.WillTests)
+            return (int) ec.resultCode;
+
 
         Console.WriteLine($"Tests started at {getTimeString(DateTime.Now)}");
 
         // ---------------- Тесты ----------------
-        ec = MainTests();
-        if (ec != ErrorCodes.Success)
+        ec.resultCode = MainTests();
+        if (ec.resultCode != ErrorCodes.Success)
         {
-            Console.WriteLine($"{getTimeString(DateTime.Now)}. Error during tests");
-            return (int) ec;
+            Console.Error.WriteLine($"{getTimeString(DateTime.Now)}. Error during tests");
+            return (int) ec.resultCode;
         }
 
-        Console.WriteLine($"Builder successfully ended at {getTimeString(DateTime.Now)}");
+        using (var opt = new NotErrorConsoleOptions())
+        {
+            Console.Write($"Builder successfully ended at {getTimeString(DateTime.Now)}");
+        }
 
         return (int) ErrorCodes.Success;
     }
@@ -43,5 +57,85 @@ partial class Program
         // Наличие файла builder.lock - это означает, что билд был неуспешным
         Builder_Lock = 1
     };
+
+    /// <summary>Класс для временного переопределения параметров консоли</summary>
+    /// <remarks>Использование:
+    /// <para>Отнаследовать класс. В конструкторе задать нужные условия. Если нужно, переопределить Disposing()</para>
+    /// Применять с ключевым словом using
+    /// <para>using var console_opts = new ConsoleOptionsChild();</para>
+    /// </remarks>
+    public abstract class ConsoleOptions: IDisposable
+    {
+                                                        /// <summary>Первоначальный цвет фона консоли</summary>
+        public ConsoleColor InitialBackgroundColor;     /// <summary>Первоначальный цвет текста консоли</summary>
+        public ConsoleColor InitialForegroundColor;
+
+        public ConsoleOptions()
+        {
+            InitialBackgroundColor = Console.BackgroundColor;
+            InitialForegroundColor = Console.ForegroundColor;
+        }
+
+        public void Dispose()
+        {
+            Disposing();
+        }
+
+        public virtual void Disposing()
+        {
+            Console.BackgroundColor = InitialBackgroundColor;
+            Console.ForegroundColor = InitialForegroundColor;
+
+            // Делаем для того, чтобы цвет фона не распространился на следующую строку
+            Console.WriteLine();
+        }
+    }
+
+    public class ErrorConsoleOptions : ConsoleOptions
+    {
+        public ErrorConsoleOptions() => Console.BackgroundColor = ConsoleColor.DarkRed;
+    }
+
+    public class NotErrorConsoleOptions : ConsoleOptions
+    {
+        public NotErrorConsoleOptions()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+
+    public class NotImportantConsoleOptions : ConsoleOptions
+    {
+        public NotImportantConsoleOptions()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+
+    public static void SetErrorHandlers()
+    {
+        builder_lock_event       += Builder_Lock_ErrorHandler;
+        updated_file_found_event += Updated_File_Found_Handler;
+    }
+
+    public static void Builder_Lock_ErrorHandler(FileInfo builder_lock_file)
+    {
+        using (var bg = new ErrorConsoleOptions())
+        {
+            Console.Error.Write("builder.lock file exists. ");
+        }
+
+        Console.Error.Write("The build was run twice or got a code error during building");
+    }
+
+    public static void Updated_File_Found_Handler(FileInfo updatedFile)
+    {
+        using (var opt = new NotErrorConsoleOptions())
+        {
+            Console.Error.Write($"Update file found: {updatedFile.FullName}");
+        }
+    }
 
 }
