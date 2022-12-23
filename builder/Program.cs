32adcff862bba5e53e05aa21bb2dd455a84d4d49
fdsc <fdsc@yandex.ru>
2022-12-23 20:19:13 +0300

    --

--- builder/Program.cs
+++ builder/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using static utils.DateTimeStrings;
+﻿using static utils.DateTimeStrings;
 
 namespace builder;
 
--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,6 @@ rootDir=`pwd`
 rm -rf ./build/
 mkdir -p ./build/builder
 
+dotnet clean
+
 bash $rootDir/build.sh
